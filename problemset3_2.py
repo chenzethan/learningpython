c41978f5dd0ed16a4b7b4a70eb1c621e167268bb
chenzethan <67557098+chenzethan@users.noreply.github.com>
2021-09-06 14:18:49 -0400

    Add files via upload

---
+++ problemset3_2.py
@@ -0,0 +1,8 @@
+nlis = [23,64,23,46,12,24]          # list
+atup = ("c","e","a","d","b")        # tuple
+str1 = "Rumplestilskin"             # string
+
+#%%
+def problem3_2(collection):
+    for i in range(0, len(collection)):
+        print(collection[i])
